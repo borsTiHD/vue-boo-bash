55d2f8a67296bab1b14cc4ef52cf8916e68d4024
borsTiHD <bastian.j@gmx.net>
2024-03-01 13:47:33 +0100

    refactor: ♻️ Refactoring templates

--- src/components/GameOver.vue
+++ src/components/GameOver.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { useGameStore } from '@/stores/game'
+
+const gameStore = useGameStore()
+</script>
+
 <template>
     <div v-if="gameStore.gameOver" class="relative m-auto p-4 bg-white shadow-lg rounded-xl flex flex-col justify-between items-center gap-2 select-none pointer-events-none">
         <span class="text-3xl font-bold">Game Over</span>
@@ -5,9 +11,3 @@
         <span class="text-2xl">Score: {{ gameStore.score }}</span>
     </div>
 </template>
-
-<script setup lang="ts">
-import { useGameStore } from '@/stores/game'
-
-const gameStore = useGameStore()
-</script>
--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,11 +1,3 @@
-<template>
-    <div v-if="show" class="relative mb-auto mt-8 p-4 bg-white shadow-lg rounded-xl flex flex-col justify-between items-center gap-2 select-none pointer-events-none">
-        <span class="text-2xl font-bold">Welcome to Boo Bash 👻</span>
-        <span class="text-2xl">Score: {{ gameStore.score }}</span>
-        <span class="text-2xl">Time: {{ gameStore.gameTime }}</span>
-    </div>
-</template>
-
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useGameStore } from '@/stores/game'
@@ -13,3 +5,11 @@ import { useGameStore } from '@/stores/game'
 const gameStore = useGameStore()
 const show = computed(() => gameStore.score > 0 && gameStore.gameTime > 0)
 </script>
+
+<template>
+    <div v-if="show" class="relative mb-auto mt-8 p-4 bg-white shadow-lg rounded-xl flex flex-col justify-between items-center gap-2 select-none pointer-events-none">
+        <span class="text-2xl font-bold">Welcome to Boo Bash 👻</span>
+        <span class="text-2xl">Score: {{ gameStore.score }}</span>
+        <span class="text-2xl">Time: {{ gameStore.gameTime }}</span>
+    </div>
+</template>
