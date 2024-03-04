84cc5166b202317ecc3571e4c5f74577475116ec
borsTiHD <bastian.j@gmx.net>
2024-03-04 10:34:55 +0100

    feat: 🚧 Optimizing build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import BooBash from '@/components/BooBash.vue'
-import type { GameSettings } from '@/types/GameSettings'
 
-export { BooBash }
-export type { GameSettings }
+export default BooBash
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue()
-    ],
+    plugins: [vue(), dts({
+        insertTypesEntry: true
+    })],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
